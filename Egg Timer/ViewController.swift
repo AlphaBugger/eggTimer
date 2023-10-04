8b8907e95e431f14a6c05fe389d5a210ef3a7c79
AlphaBugger <zepciuceduard@gmail.com>
2023-10-04 19:40:00 +0300

    First commit

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timer: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        startTimer(eggType: sender.currentTitle!)
+    }
+    
+    
+    func startTimer(eggType: String){
+        if eggType == "Soft"{
+            timer.progress = 0.25
+        }
+        else
+        {
+            if eggType == "Medium"{
+                timer.progress = 0.5
+            }
+            else{
+                if eggType == "Hard"{
+                    timer.progress = 1
+            }
+            
+            }
+        }
+        
+    }
 }
 
